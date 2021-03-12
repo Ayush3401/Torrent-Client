fd5309d10ce475de118b1dadf784d17425e7c27e
Ayush3401 <ayushgupta3401@gmail.com>
2021-03-12 23:13:15 +0530

    added torrent file

---
+++ index.js
@@ -0,0 +1,9 @@
+/** @format */
+
+"use strict";
+const fs = require("fs");
+const bencode = require("bencode");
+
+const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
+
+console.log(torrent);
